8a661b00038dfa24ebb5d10ef4b3f6919ef43abb
claytoncalixto <claytonrcalixto@gmail.com>
2021-05-13 19:05:24 -0300

    Project created

---
+++ backend/src/test/java/com/claytoncalixto/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claytoncalixto.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
