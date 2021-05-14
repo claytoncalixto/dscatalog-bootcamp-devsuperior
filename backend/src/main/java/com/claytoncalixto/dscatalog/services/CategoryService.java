17443890ae8e6bea635310c123cca80281ad982d
claytoncalixto <claytonrcalixto@gmail.com>
2021-05-14 17:54:52 -0300

    DTO

--- backend/src/main/java/com/claytoncalixto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/claytoncalixto/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.claytoncalixto.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.claytoncalixto.dscatalog.dto.CategoryDTO;
 import com.claytoncalixto.dscatalog.entities.Category;
 import com.claytoncalixto.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();		
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());		
 	}
 }
