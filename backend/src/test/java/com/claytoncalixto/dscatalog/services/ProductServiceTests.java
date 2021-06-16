eb36f1b8c7cef3060b47f05bbb7f4ac3e79b8ed1
claytoncalixto <claytonrcalixto@gmail.com>
2021-06-15 21:14:44 -0300

    02-26 Test delete throws DatabaseException when id dependent

--- backend/src/test/java/com/claytoncalixto/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/claytoncalixto/dscatalog/services/ProductServiceTests.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.claytoncalixto.dscatalog.repositories.ProductRepository;
+import com.claytoncalixto.dscatalog.services.exceptions.DatabaseException;
 import com.claytoncalixto.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class)
@@ -24,18 +26,32 @@ public class ProductServiceTests {
 	
 	private long existingId;
 	private long nonExistingId;
+	private long dependentId;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
+		dependentId = 4L;
 		
 		Mockito.doNothing().when(repository).deleteById(existingId);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+	}
+	
+	@Test
+	public void deleteShouldThrowDatabaseExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			service.delete(dependentId);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId);
 	}
 	
 	@Test
 	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+		
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.delete(nonExistingId);
 		});
@@ -45,6 +61,7 @@ public class ProductServiceTests {
 	
 	@Test
 	public void deleteShouldDoNothingWhenIdExists() {
+		
 		Assertions.assertDoesNotThrow(() -> {
 			service.delete(existingId);
 		});
