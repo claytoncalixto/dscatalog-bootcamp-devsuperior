8c8355416e0d05d27223e86081fe8fc85785488e
claytoncalixto <claytonrcalixto@gmail.com>
2021-07-02 22:44:19 -0300

    03-07 CRUD of users PART 2

--- backend/src/main/java/com/claytoncalixto/dscatalog/services/UserService.java
+++ backend/src/main/java/com/claytoncalixto/dscatalog/services/UserService.java
@@ -9,11 +9,13 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.claytoncalixto.dscatalog.dto.RoleDTO;
 import com.claytoncalixto.dscatalog.dto.UserDTO;
+import com.claytoncalixto.dscatalog.dto.UserInsertDTO;
 import com.claytoncalixto.dscatalog.entities.Role;
 import com.claytoncalixto.dscatalog.entities.User;
 import com.claytoncalixto.dscatalog.repositories.RoleRepository;
@@ -23,6 +25,9 @@ import com.claytoncalixto.dscatalog.services.exceptions.ResourceNotFoundExceptio
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private UserRepository repository;
@@ -44,9 +49,10 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO insert(UserDTO dto) {
+	public UserDTO insert(UserInsertDTO dto) {
 		User entity = new User();
 		copyDtoToEntity(dto, entity);
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
 		entity = repository.save(entity);
 		return new UserDTO(entity);
 	}
