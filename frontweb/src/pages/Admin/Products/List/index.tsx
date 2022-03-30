f985a7d8b8c9a83bec2ecb06b2f5dfe43cc9785a
claytoncalixto <claytonrcalixto@gmail.com>
2022-03-30 19:44:52 -0300

    Finishing ProductCrudCard mobile layout

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -19,6 +19,10 @@ const List = () => {
         name: 'Eletronicos',
       },
       {
+        id: 2,
+        name: 'Celulares',
+      },
+      {
         id: 3,
         name: 'Computadores',
       },
