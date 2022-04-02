d8196fd5c68802629dd419a801ab1f3c26c4c6ad
claytoncalixto <claytonrcalixto@gmail.com>
2022-04-02 14:24:48 -0300

    Delete product, observer pattern

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -1,13 +1,33 @@
-import './styles.css';
-
 import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
 import CategoryBadge from '../CategoryBadge';
 import { Link } from 'react-router-dom';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
-type Props = { product: Product };
+import './styles.css';
 
-const ProductCrudCard = ({ product }: Props) => {
+type Props = { 
+  product: Product;
+  onDelete: Function;
+};
+
+const ProductCrudCard = ({ product, onDelete }: Props) => {
+  const handleDelete = (productId: number) => {
+
+if(!window.confirm("Tem certeza que deseja deletar?")){
+  return;
+}
+    const config: AxiosRequestConfig = {
+      method: 'DELETE',
+      url: `/products/${productId}`,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then(() => {
+      onDelete();
+    });
+  };
   return (
     <div className="base-card product-crud-card">
       <div className="product-crud-card-top-container">
@@ -19,16 +39,23 @@ const ProductCrudCard = ({ product }: Props) => {
           <ProductPrice price={product.price} />
         </div>
         <div className="product-crud-categories-container">
-          {
-            product.categories.map((category) => (
-              <CategoryBadge name={category.name} key={category.id} />
-            ))}
-        </div> 
+          {product.categories.map((category) => (
+            <CategoryBadge name={category.name} key={category.id} />
+          ))}
+        </div>
       </div>
       <div className="product-crud-card-buttons-container">
-        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">EXCLUIR</button>
-        <Link to={`/admin/products/${product.id}`}><button className="btn btn-outline-secondary product-crud-card-button">EDITAR</button></Link>
-        
+        <button
+          onClick={() => handleDelete(product.id)}
+          className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first"
+        >
+          EXCLUIR
+        </button>
+        <Link to={`/admin/products/${product.id}`}>
+          <button className="btn btn-outline-secondary product-crud-card-button">
+            EDITAR
+          </button>
+        </Link>
       </div>
     </div>
   );
